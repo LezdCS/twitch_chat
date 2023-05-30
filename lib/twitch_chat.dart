b6728951e459c03ddbc0b8a2d82637dbb544ee49
Lezd <lilinouxgaming@gmail.com>
2023-05-30 22:35:47 +0900

    export all chat_events

--- lib/twitch_chat.dart
+++ lib/twitch_chat.dart
@@ -3,5 +3,11 @@ library twitch_chat;
 export 'src/twitch_chat.dart';
 
 export 'src/chat_message.dart';
+export 'src/chat_events/announcement.dart';
+export 'src/chat_events/bit_donation.dart';
+export 'src/chat_events/incoming_raid.dart';
+export 'src/chat_events/reward_redemption.dart';
+export 'src/chat_events/sub_gift.dart';
+export 'src/chat_events/subscription.dart';
 
 export 'src/data/twitch_api.dart';
